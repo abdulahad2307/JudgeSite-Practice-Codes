02549bf53b10485e5bfa82bbff7b7587d0610a9d
abdulahad2307 <abdul.ahad@northsouth.edu>
2020-08-09 23:27:44 -1100

    PS_SQL_BasicSelect End

--- SQL/Basic Select.sql
+++ SQL/Basic Select.sql
@@ -74,3 +74,75 @@ SELECT CITY, LENGTH(CITY) AS `length`
     WHERE 1
     ORDER BY `length` DESC, CITY ASC
     LIMIT 1
+
+## Weather Observation Station 6:
+Query the list of CITY names starting with vowels (i.e., a, e, i, o, or u) from STATION. Your result cannot contain duplicates.
+
+SELECT CITY 
+FROM STATION 
+WHERE CITY RLIKE '^[AEIOUaeiou].*$'
+
+## Weather Observation Station 7:
+Query the list of CITY names ending with vowels (a, e, i, o, u) from STATION. Your result cannot contain duplicates.
+
+SELECT DISTINCT CITY 
+FROM STATION 
+WHERE RIGHT(CITY,1) IN ('a','i','e','o','u');
+
+## Weather Observation Station 8:
+Query the list of CITY names from STATION which have vowels (i.e., a, e, i, o, and u) as both their first and last characters. Your result cannot contain duplicates.
+
+SELECT DISTINCT CITY 
+FROM STATION WHERE LEFT(CITY,1) IN ('a','i','e','o','u','A','E','I','O','U')  and RIGHT(CITY,1) IN ('a','i','e','o','u')
+
+## Weather Observation Station 9:
+Query the list of CITY names from STATION that do not start with vowels. Your result cannot contain duplicates.
+
+SELECT DISTINCT CITY 
+FROM STATION 
+WHERE LEFT(CITY,1) NOT IN ('A','E','I','O','U')
+
+## Weather Observation Station 10:
+Query the list of CITY names from STATION that do not end with vowels. Your result cannot contain duplicates.
+
+SELECT DISTINCT CITY
+FROM STATION
+WHERE RIGHT(CITY,1) NOT IN ('a','e','i','o','u')
+
+## Weather Observation Station 11:
+Query the list of CITY names from STATION that either do not start with vowels or do not end with vowels. Your result cannot contain duplicates.
+
+SELECT DISTINCT CITY
+FROM STATION
+WHERE LEFT(CITY,1) NOT IN ('A','E','I','O','U') or RIGHT(CITY,1) NOT IN ('a','e','i','o','u')
+
+## Weather Observation Station 12:
+Query the list of CITY names from STATION that do not start with vowels and do not end with vowels. Your result cannot contain duplicates.
+
+SELECT DISTINCT CITY 
+FROM STATION 
+WHERE CITY NOT REGEXP '^[aeiou]|[aeiou]$'
+
+## Higher Than 75 Marks:
+Query the Name of any student in STUDENTS who scored higher than 75 Marks. Order your output by the last three characters of each name. If two or more students both have names ending in the same last three characters (i.e.: Bobby, Robby, etc.), secondary sort them by ascending ID.
+
+SELECT Name
+FROM STUDENTS
+WHERE Marks > 75
+ORDER BY RIGHT(Name, 3), ID
+
+## Employee Names:
+Write a query that prints a list of employee names (i.e.: the name attribute) from the Employee table in alphabetical order.
+
+SELECT name
+FROM Employee
+ORDER BY name
+
+## Employee Salaries:
+Write a query that prints a list of employee names (i.e.: the name attribute) for employees in Employee having a salary greater than $2000 per month who have been employees for less than 10 months. Sort your result by ascending employee_id.
+
+SELECT name
+FROM Employee
+WHERE salary > 2000 and months < 10
+ORDER BY employee_id
+
